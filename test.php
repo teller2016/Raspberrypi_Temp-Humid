c8fec01fe634a941c1f880d7c197bda1f300adb2
JungSubeom <teller75@naver.com>
2021-07-01 16:57:30 +0900

    Show data by Table

--- test.php
+++ test.php
@@ -6,17 +6,41 @@
     
     $conn = mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_db);
     
-    echo("Time,Humid,Temp");
-    echo("<br/>");
+    
+    
+    
     
     
     $query = "SELECT * FROM collect_data ORDER BY collect_time DESC";
     $result = mysqli_query($conn,$query);
     
-    while($row = mysqli_fetch_array($result))
+    echo("<table border=3 align=center>
+    <caption>온습도계 측정</caption>
+    <tr>
+		<th>측정 시간</th>
+		<th>습도</th>
+		<th>온도</th>
+    </tr>
+		");
+		
+	while($row = mysqli_fetch_array($result))
     {
         $secTOTime = gmdate('H:i:s',$row[collect_time]);
-        echo("{$secTOTime}, {$row['value2']}, {$row['value1']}");
-        echo("<br/>");
+        
+        echo("<tr>");
+        
+        echo("<td>". $secTOTime ."</td>");
+        echo("<td>". $row['value2'] ."%</td>");
+        echo("<td>". $row['value1'] ."℃</td>");
+        
+        
+        echo("</tr>");
+        
+        
     }
+    
+		
+	echo("</table>");
+
+    
 ?>
